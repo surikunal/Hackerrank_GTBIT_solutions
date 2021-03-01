eb7efe272da0e167dd74c6e3c1e3e6e2a4f22cf8
surikunal <suri.kunal.222@gmail.com>
2021-03-01 18:22:43 +0530

    update

--- page_2/conversions.java
+++ page_2/conversions.java
@@ -1,10 +1,9 @@
-import java.io.*;
-import java.util.*;
+/* import java.util.*;
 
 public class conversions {
+    public static Scanner scn = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
         int sn = scn.nextInt();
         int sb = scn.nextInt();
         int db = scn.nextInt();
@@ -41,5 +40,77 @@ public class conversions {
             power *= 10;
         }
         return ans;
+    }
+} */
+
+import java.util.*;
+
+public class conversions {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int t = scn.nextInt();
+        for (int i = 0; i < t; i++) {
+            int n = scn.nextInt();
+            int k = scn.nextInt();
+            int[][] arr = new int[n][2];
+            for (int j = 0; j < n; j++) {
+                arr[j][0] = scn.nextInt();
+                arr[j][1] = scn.nextInt();
+            }
+            System.out.println(maxRange(k, arr));
+        }
+    }
+
+    public static int maxRange(int k, int[][] arr) {
+        int[] ans = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            int si = arr[i][0];
+            int ei = arr[i][1];
+
+            if (si >= 0 && si < arr.length) {
+                ans[si]++;
+            }
+
+            if (ei + 1 >= 0 && ei + 1 < arr.length) {
+                ans[ei + 1]--;
+            }
+        }
+
+        int[] psak = new int[ans.length];
+        int[] psakp1 = new int[ans.length];
+
+        if (ans[0] == k) {
+            psak[0]++;
+        }
+        if (ans[0] == k + 1) {
+            psakp1[0]++;
+        }
+
+        for (int i = 1; i < ans.length; i++) {
+            ans[i] = ans[i] + ans[i - 1];
+            if (ans[i] == k) {
+                psak[i]++;
+            }
+            if (ans[i] == k + 1) {
+                psakp1[i]++;
+            }
+        }
+
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            int si = arr[i][0];
+            int ei = arr[i][1];
+
+            int loss = 0, profit = 0;
+            if (si - 1 >= 0 && ei < arr.length) {
+                loss = psak[ei] - psak[si - 1];
+            }
+            if (si - 1 >= 0 && ei < arr.length) {
+                profit = psakp1[ei] - psakp1[si - 1];
+            }
+            max = Math.max(max, ans[i] - loss + profit);
+        }
+
+        return max;
     }
 }
